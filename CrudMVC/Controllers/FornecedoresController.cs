be730a06b4a55032fd95445e2cecb0043e069df6
Marcos <sbmarcos777@gmail.com>
2023-07-27 23:15:16 -0300

    Adicionando Fornecedores

---
+++ CrudMVC/Controllers/FornecedoresController.cs
@@ -0,0 +1,64 @@
+﻿using CrudMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudMVC.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        private readonly Context _context;
+
+        public FornecedoresController(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Fornecedores.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult NovoFornecedor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NovoFornecedor(Fornecedor fornecedor)
+        {
+            await _context.Fornecedores.AddAsync(fornecedor);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AtualizarFornecedor(int fornecedorId)
+        {
+            Fornecedor fornecedor = await _context.Fornecedores.FindAsync(fornecedorId);
+
+            return View(fornecedor);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AtualizarFornecedor(Fornecedor fornecedor)
+        {
+            _context.Fornecedores.Update(fornecedor);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ExcluirFornecedor(int fornecedorId)
+        {
+            Fornecedor fornecedor = await _context.Fornecedores.FindAsync(fornecedorId);
+            _context.Fornecedores.Remove(fornecedor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
